c4a6ef745a31fd24a581aec95d30659c98f9f089
Erwin Bonsma <eriban@users.sourceforge.net>
2020-01-22 23:28:50 +0100

    Add roll tracker

---
+++ Assets/Scripts/OrientRollTracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrientRollTracker : MonoBehaviour {
+
+    float maxAngle = 20.0f;
+
+    PlayerMovement playerMovement;
+
+    void Start() {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        playerMovement = player.GetComponent<PlayerMovement>();
+        playerMovement.onRollChange += onRollChange;
+    }
+
+    void onRollChange(PlayerMovement playerMovement) {
+        float angle = maxAngle * -playerMovement.RollTurnAmount;
+
+        transform.localEulerAngles = new Vector3(0, 0, angle);
+    }
+
+}
