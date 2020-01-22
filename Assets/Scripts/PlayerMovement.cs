00cb50a9fe618427aae8aa0c801b5ac971c4f3c3
Erwin Bonsma <eriban@users.sourceforge.net>
2020-01-22 17:49:33 +0100

    Add ability to loop and twist

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,34 +4,77 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     public int numSideTurnSteps;
+    public int numUpTurnSteps;
+
     public float maxSideTurnAngle;
+    public float maxUpTurnAngle;
+    public float maxTwistTurnAngle;
+
     public float speed;
 
     // Ranges from [-numSideTurnSteps, numSideTurnSteps]
-    int sideTurn = 0;
+    int sideTurn;
 
-    // Start is called before the first frame update
-    void Start() {
+    // Ranges from [-1, maxUpTurnSteps]
+    int upTurn;
+
+    // Ranges from [-1, 1]
+    int twistTurn;
+
+    void ProcessInput() {
+        bool shiftPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            if (shiftPressed) {
+                if (twistTurn >= 0) {
+                    twistTurn--;
+                }
+            } else {
+                if (sideTurn > -numSideTurnSteps) {
+                    sideTurn--;
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+            if (shiftPressed) {
+                if (twistTurn <= 0) {
+                    twistTurn++;
+                }
+            } else {
+                if (sideTurn < numSideTurnSteps) {
+                    sideTurn++;
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W) && upTurn < numUpTurnSteps) {
+            upTurn++;
+        }
+        if (Input.GetKeyDown(KeyCode.S) && upTurn >= 0) {
+            upTurn--;
+        }
+    }
+
+    void UpdateRotation() {
+        if (sideTurn != 0) {
+            float turnAngle = sideTurn * maxSideTurnAngle / numSideTurnSteps;
+            transform.RotateAround(transform.position, transform.up, turnAngle);
+            Debug.Log("sideTurn = " + sideTurn);
+        }
+        if (upTurn != 0) {
+            float turnAngle = upTurn * maxUpTurnAngle / numUpTurnSteps;
+            transform.RotateAround(transform.position, transform.right, -turnAngle);
+            Debug.Log("upTurn = " + upTurn);
+        }
+        if (twistTurn != 0) {
+            float turnAngle = twistTurn * maxTwistTurnAngle;
+            transform.RotateAround(transform.position, transform.forward, -turnAngle);
+            Debug.Log("twistTurn = " + twistTurn);
+        }
     }
 
     void FixedUpdate() {
-        if (Input.GetKeyDown(KeyCode.A) && sideTurn > -numSideTurnSteps) {
-            sideTurn--;
-            Debug.Log("sideTurn = " + sideTurn);
-        }
-        if (Input.GetKeyDown(KeyCode.D) && sideTurn < numSideTurnSteps) {
-            sideTurn++;
-            Debug.Log("sideTurn = " + sideTurn);
-        }
-
-        if (sideTurn != 0) {
-            float turnAngle = sideTurn * maxSideTurnAngle;
-
-            Vector3 rotV = transform.localEulerAngles;
-            rotV.y += turnAngle * Time.deltaTime;
-            transform.localEulerAngles = rotV;
-        }
-        // Debug.Log("forward = " + transform.forward);
+        ProcessInput();
+        UpdateRotation();
 
         transform.position += transform.forward * Time.deltaTime * speed;
     }
