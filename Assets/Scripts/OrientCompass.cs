df31859451ad6f694742507e2060acb11984d6c6
Erwin Bonsma <eriban@users.sourceforge.net>
2020-01-22 20:41:10 +0100

    Add compass

---
+++ Assets/Scripts/OrientCompass.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrientCompass : MonoBehaviour {
+    void Update() {
+        // Always keep same orientation wrt to the world
+        transform.rotation = Quaternion.identity;
+    }
+}
