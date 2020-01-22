e8686302f19d56895acb82f614774c58e3b6b7a6
Erwin Bonsma <eriban@users.sourceforge.net>
2020-01-22 17:04:16 +0100

    Let camera optionally follow player's position

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour {
     public GameObject target;
+    public bool isPositionFixed;
 
     void Update() {
-        if (target != null) {
+        if (target == null) {
+            return;
+        }
+
+        if (isPositionFixed) {
             Vector3 lookAt = target.transform.position - transform.position;
             transform.rotation = Quaternion.LookRotation(lookAt);
+        } else {
+            transform.position = target.transform.position - target.transform.forward + target.transform.up * 0.3f;
+            transform.rotation = target.transform.rotation;
         }
     }
 }
