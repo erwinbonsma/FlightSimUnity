ff1f207430f4c936786e094c3eecb8c93d0d5f7f
Erwin Bonsma <eriban@users.sourceforge.net>
2020-01-22 18:08:16 +0100

    Enabled smoother turning

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,66 +10,74 @@ public class PlayerMovement : MonoBehaviour {
     public float maxUpTurnAngle;
     public float maxTwistTurnAngle;
 
+    public float turnSpeed = 0.4f;
+
     public float speed;
 
     // Ranges from [-numSideTurnSteps, numSideTurnSteps]
-    int sideTurn;
+    int targetSideTurn;
+    float sideTurn;
 
     // Ranges from [-1, maxUpTurnSteps]
-    int upTurn;
+    int targetUpTurn;
+    float upTurn;
 
     // Ranges from [-1, 1]
-    int twistTurn;
+    int targetTwistTurn;
+    float twistTurn;
 
     void ProcessInput() {
         bool shiftPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 
         if (Input.GetKeyDown(KeyCode.A)) {
             if (shiftPressed) {
-                if (twistTurn >= 0) {
-                    twistTurn--;
+                if (targetTwistTurn >= 0) {
+                    targetTwistTurn--;
                 }
             } else {
-                if (sideTurn > -numSideTurnSteps) {
-                    sideTurn--;
+                if (targetSideTurn > -numSideTurnSteps) {
+                    targetSideTurn--;
                 }
             }
         }
         if (Input.GetKeyDown(KeyCode.D)) {
             if (shiftPressed) {
-                if (twistTurn <= 0) {
-                    twistTurn++;
+                if (targetTwistTurn <= 0) {
+                    targetTwistTurn++;
                 }
             } else {
-                if (sideTurn < numSideTurnSteps) {
-                    sideTurn++;
+                if (targetSideTurn < numSideTurnSteps) {
+                    targetSideTurn++;
                 }
             }
         }
-        if (Input.GetKeyDown(KeyCode.W) && upTurn < numUpTurnSteps) {
-            upTurn++;
+        if (Input.GetKeyDown(KeyCode.W)) {
+            if (targetUpTurn < numUpTurnSteps) {
+                targetUpTurn++;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.S) && upTurn >= 0) {
-            upTurn--;
+        if (Input.GetKeyDown(KeyCode.S)) {
+            if (targetUpTurn >= 0) {
+                targetUpTurn--;
+            }
         }
     }
 
     void UpdateRotation() {
-        if (sideTurn != 0) {
-            float turnAngle = sideTurn * maxSideTurnAngle / numSideTurnSteps;
-            transform.RotateAround(transform.position, transform.up, turnAngle);
-            Debug.Log("sideTurn = " + sideTurn);
-        }
-        if (upTurn != 0) {
-            float turnAngle = upTurn * maxUpTurnAngle / numUpTurnSteps;
-            transform.RotateAround(transform.position, transform.right, -turnAngle);
-            Debug.Log("upTurn = " + upTurn);
-        }
-        if (twistTurn != 0) {
-            float turnAngle = twistTurn * maxTwistTurnAngle;
-            transform.RotateAround(transform.position, transform.forward, -turnAngle);
-            Debug.Log("twistTurn = " + twistTurn);
-        }
+        float turnAngle;
+
+        sideTurn = Mathf.Lerp(sideTurn, targetSideTurn, turnSpeed);
+        upTurn = Mathf.Lerp(upTurn, targetUpTurn, turnSpeed);
+        twistTurn = Mathf.Lerp(twistTurn, targetTwistTurn, turnSpeed);
+
+        turnAngle = sideTurn * maxSideTurnAngle / numSideTurnSteps;
+        transform.RotateAround(transform.position, transform.up, turnAngle);
+
+        turnAngle = upTurn * maxUpTurnAngle / numUpTurnSteps;
+        transform.RotateAround(transform.position, transform.right, -turnAngle);
+
+        turnAngle = twistTurn * maxTwistTurnAngle;
+        transform.RotateAround(transform.position, transform.forward, -turnAngle);
     }
 
     void FixedUpdate() {
