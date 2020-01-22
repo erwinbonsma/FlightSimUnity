3eeff4930d4fec40b4b4c46e029b957126cf8045
Erwin Bonsma <eriban@users.sourceforge.net>
2020-01-22 15:40:02 +0100

    Let camera follow player (rotation only)

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour {
+    public GameObject target;
+
+    void Update() {
+        if (target != null) {
+            Vector3 lookAt = target.transform.position - transform.position;
+            transform.rotation = Quaternion.LookRotation(lookAt);
+        }
+    }
+}
