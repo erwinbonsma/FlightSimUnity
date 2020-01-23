f5afee6b212768e19a2abcd4eeeef34d37f6d78b
Erwin Bonsma <eriban@users.sourceforge.net>
2020-01-23 13:47:12 +0100

    Add main menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    public void StartGame() {
+        Debug.Log("Starting Game");
+
+        StartCoroutine(AsyncLoadScene("SampleScene"));
+    }
+
+    IEnumerator AsyncLoadScene(string sceneName) {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone) {
+            yield return null;
+        }
+    }
+}
