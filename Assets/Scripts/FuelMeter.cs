b6146354398e3cf6f87c940ba941d3284c710f4e
Erwin Bonsma <eriban@users.sourceforge.net>
2020-01-27 12:43:55 +0100

    Added fuel meter

---
+++ Assets/Scripts/FuelMeter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelMeter : MonoBehaviour {
+
+    public float fuelConsumption = 0.01f;
+
+    float fuelLevel;
+
+    // Start is called before the first frame update
+    void Start() {
+        fuelLevel = 1f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        fuelLevel -= fuelConsumption * Time.deltaTime;
+        Debug.Log("Fuel level = " + fuelLevel);
+        transform.localEulerAngles = new Vector3(0, 0, 90 - fuelLevel * 180);
+    }
+}
