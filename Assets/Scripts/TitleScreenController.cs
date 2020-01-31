3a7d6bc39e9efc100ddad907fe5d08454f1cfb45
Erwin Bonsma <eriban@users.sourceforge.net>
2020-01-31 12:10:30 +0100

    Add title screen

---
+++ Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TitleScreenController : MonoBehaviour {
+
+    void Start() {
+        StartCoroutine(WaitThenShowMainMenu());
+    }
+
+    void Update() {
+        if (Input.anyKey) {
+            ShowMainMenu();
+        }
+    }
+
+    IEnumerator WaitThenShowMainMenu() {
+        yield return new WaitForSeconds(4f);
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu() {
+        if (enabled) {
+            StartCoroutine(UnityUtil.AsyncLoadScene("MainMenu"));
+            enabled = false;
+        }
+    }
+}
